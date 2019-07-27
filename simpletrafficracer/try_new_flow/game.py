859594e0a860045e926b02e8a3e2da8de68ac38f
Stefano Probst <senden9@gmail.com>
2019-07-27 11:56:42 +0200

    More formating

--- simpletrafficracer/try_new_flow/game.py
+++ simpletrafficracer/try_new_flow/game.py
@@ -1,4 +1,8 @@
-import pygame, random, sys ,os,time
+import pygame
+import random
+import sys
+import os
+import time
 from pygame.locals import *
 from matplotlib.pyplot import imshow
 from PIL import Image
@@ -14,7 +18,7 @@ import math
 import resource
 from enum import Enum
 
-# set SDL to use the dummy NULL video driver, 
+# set SDL to use the dummy NULL video driver,
 #   so it doesn't need a windowing system.
 #os.environ["SDL_VIDEODRIVER"] = "dummy"
 
@@ -26,9 +30,8 @@ from tensorflow import keras
 import numpy as np
 
 
-
 # reinforce
-#def replay(batch_size):
+# def replay(batch_size):
 #        minibatch = random.sample(memory, batch_size)
 #        for state, own_pos, action, done, points in minibatch:
 #            target = points
@@ -37,7 +40,7 @@ import numpy as np
 #            target_f[0][action] = target
 #            model.fit(reshaped_state, target_f, epochs=1, verbose=0)
 #
-#def calc_points(state, done):
+# def calc_points(state, done):
 #    target = 0
 #    found_points = 0
 #    objects = np.nonzero(state)
@@ -62,7 +65,7 @@ import numpy as np
 #    return target
 
 # build convnet
-#def build_model():
+# def build_model():
 #    # Neural Net for Deep-Q learning Model
 #
 #    #np.random.seed(1000)
@@ -114,6 +117,7 @@ class FrontBack(Enum):
     NEUTRAL = 0
     BACKWARD = -1
 
+
 class LeftRight(Enum):
     LEFT = 1
     NEUTRAL = 0
@@ -122,6 +126,7 @@ class LeftRight(Enum):
 
 class GameDing:
     TOPSCORE_FILE = "data/save.dat"
+
     def __init__(self):
         # "Settings"
         self.WINDOWWIDTH = 800
@@ -133,23 +138,25 @@ class GameDing:
         self.BADDIEMAXSPEED = 8
         self.ADDNEWBADDIERATE = 6
         self.PLAYERMOVERATE = 5
-        self.REPLAY_MEMORY=10000 # Images to save for replay
-        self.OBSERVE = 5000. # timesteps to observe before training
-        self.EXPLORE = 5000. # frames over which to anneal epsilon
-        self.ACTIONS=4
+        self.REPLAY_MEMORY = 10000  # Images to save for replay
+        self.OBSERVE = 5000.  # timesteps to observe before training
+        self.EXPLORE = 5000.  # frames over which to anneal epsilon
+        self.ACTIONS = 4
         self.memory = deque(maxlen=self.REPLAY_MEMORY)
         self.gamma = 0.95  # discount rate
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
         self.learning_rate = 0.001
-        self.FINAL_EPSILON = 0.05 # final value of epsilon
-        self.INITIAL_EPSILON = 1.0 # starting value of epsilon
-        self.GAMMA = 0.99 # decay rate of past observations
+        self.FINAL_EPSILON = 0.05  # final value of epsilon
+        self.INITIAL_EPSILON = 1.0  # starting value of epsilon
+        self.GAMMA = 0.99  # decay rate of past observations
         # Dimensions of our position board
         self.scale_down_factor = 10
-        self.max_diff_x = math.floor(self.WINDOWHEIGHT / self.scale_down_factor)+self.scale_down_factor
-        self.max_diff_y = math.floor(self.WINDOWWIDTH / self.scale_down_factor)+self.scale_down_factor
+        self.max_diff_x = math.floor(
+            self.WINDOWHEIGHT / self.scale_down_factor)+self.scale_down_factor
+        self.max_diff_y = math.floor(
+            self.WINDOWWIDTH / self.scale_down_factor)+self.scale_down_factor
         # Images
         self.playerImage = pygame.image.load('image/car1.png')
         self.car3 = pygame.image.load('image/car3.png')
@@ -161,36 +168,39 @@ class GameDing:
         self.wallRight = pygame.image.load('image/right.png')
         # Score
         if not os.path.exists(self.TOPSCORE_FILE):
-            f=open(self.TOPSCORE_FILE,'w')
+            f = open(self.TOPSCORE_FILE, 'w')
             f.write(str(0))
-            f.close()   
-        v = open(self.TOPSCORE_FILE,'r')
+            f.close()
+        v = open(self.TOPSCORE_FILE, 'r')
         self.topScore = int(v.readline())
         v.close()
         # set up pygame, the window, and the mouse cursor
         pygame.init()
         self.mainClock = pygame.time.Clock()
-        self.windowSurface = pygame.display.set_mode((self.WINDOWWIDTH, self.WINDOWHEIGHT))
+        self.windowSurface = pygame.display.set_mode(
+            (self.WINDOWWIDTH, self.WINDOWHEIGHT))
         pygame.display.set_caption('car race')
         pygame.mouse.set_visible(False)
         # fonts
         self.font = pygame.font.SysFont(None, 30)
         # V
-        self.t=0
+        self.t = 0
         # Set all resetable states
         self.reset()
 
     # Reset state for a new round
     def reset(self):
         self.baddies = []
-        #model.save(f"models/model_t{t:08}.h5")
+        # model.save(f"models/model_t{t:08}.h5")
         self.score = 0
-        self.playerRect.topleft = (self.WINDOWWIDTH / 2, self.WINDOWHEIGHT - 50)
+        self.playerRect.topleft = (
+            self.WINDOWWIDTH / 2, self.WINDOWHEIGHT - 50)
         self.moveLeft = self.moveRight = self.moveUp = self.moveDown = False
         self.baddieAddCounter = 0
         self.terminal = False
-        self.position_board = np.zeros((self.max_diff_x*2, self.max_diff_y*2), np.bool)
-    
+        self.position_board = np.zeros(
+            (self.max_diff_x*2, self.max_diff_y*2), np.bool)
+
     # game termination
     def _terminate(self):
         pygame.quit()
@@ -211,7 +221,7 @@ class GameDing:
 
     # one step forward
     def next_round(self, direction: LeftRight, gas: FrontBack, realtime=False):
-        self.score += 1 # increase score
+        self.score += 1  # increase score
         if realtime:
             self.mainClock.tick(self.FPS)
         else:
@@ -228,7 +238,7 @@ class GameDing:
             self.moveRight = False
         else:
             raise ValueError()
-        
+
         if gas == FrontBack.FORWARD:
             self.moveUp = True
             self.moveDown = False
@@ -247,7 +257,6 @@ class GameDing:
             if event.type == KEYUP:
                 if event.key == K_ESCAPE:
                     self._terminate()
-
 
         # Add new baddies at the top of the screen
         self.baddieAddCounter += 1
@@ -283,8 +292,10 @@ class GameDing:
         self.position_board.fill(False)
         for b in self.baddies:
             b['rect'].move_ip(0, b['speed'])
-            relative_position = (self.playerRect.center[0] - b["rect"].center[0], self.playerRect.center[1] - b["rect"].center[1])
-            relative_position = (math.floor(relative_position[0]/self.scale_down_factor), math.floor(relative_position[1]/self.scale_down_factor))
+            relative_position = (
+                self.playerRect.center[0] - b["rect"].center[0], self.playerRect.center[1] - b["rect"].center[1])
+            relative_position = (math.floor(
+                relative_position[0]/self.scale_down_factor), math.floor(relative_position[1]/self.scale_down_factor))
             if abs(relative_position[0]) > self.max_diff_x:
                 self.max_diff_x = abs(relative_position[0])
                 print("New max diff:", self.max_diff_x, self.max_diff_y)
@@ -293,7 +304,8 @@ class GameDing:
                 self.max_diff_y = abs(relative_position[1])
                 print("New max diff:", self.max_diff_x, self.max_diff_y)
                 sys.exit(1)
-            self.position_board[relative_position[0] + self.max_diff_x, relative_position[1] + self.max_diff_y] = True
+            self.position_board[relative_position[0] + self.max_diff_x,
+                                relative_position[1] + self.max_diff_y] = True
 
         for b in self.baddies[:]:
             if b['rect'].top > self.WINDOWHEIGHT:
@@ -303,8 +315,10 @@ class GameDing:
         self.windowSurface.fill(self.BACKGROUNDCOLOR)
 
         # Draw the score and top score.
-        self._drawText('Score: %s' % (self.score), self.font, self.windowSurface, 128, 0)
-        self._drawText('Top Score: %s' % (self.topScore), self.font, self.windowSurface, 128, 20)
+        self._drawText('Score: %s' % (self.score),
+                       self.font, self.windowSurface, 128, 0)
+        self._drawText('Top Score: %s' % (self.topScore),
+                       self.font, self.windowSurface, 128, 20)
         #drawText('Rest Life: %s' % (count), font, windowSurface, 128, 40)
 
         self.windowSurface.blit(self.playerImage, self.playerRect)
@@ -330,27 +344,30 @@ class GameDing:
 
         own_pos = (self.playerRect.x, self.playerRect.y)
 
-        im = 255 * (self.position_board / 1) # Divide with max value. Bool => 1
+        # Divide with max value. Bool => 1
+        im = 255 * (self.position_board / 1)
         w, h = im.shape
         ret = np.empty((w, h, 3), dtype=np.uint8)
         ret[:, :, 2] = ret[:, :, 1] = ret[:, :, 0] = im
         surf = pygame.surfarray.make_surface(ret)
-        self.windowSurface.blit(surf, (0,0))
+        self.windowSurface.blit(surf, (0, 0))
         pygame.display.update()
 
-        print("own pos:", own_pos, "Len:", len(self.baddies), "terminal:", self.terminal, "score:", self.score)
+        print("own pos:", own_pos, "Len:", len(self.baddies),
+              "terminal:", self.terminal, "score:", self.score)
         if self.score != 0:
-            self.memory.append((im, own_pos, (direction, gas), self.terminal, self.score))
+            self.memory.append(
+                (im, own_pos, (direction, gas), self.terminal, self.score))
 
         while len(self.memory) > self.REPLAY_MEMORY:
             self.memory.popleft()
-        
-        self.t+=1
+
+        self.t += 1
 
         if len(self.baddies) >= 40:
-            print("More then 40 other cars:",len(self.baddies))
-        
+            print("More then 40 other cars:", len(self.baddies))
+
         return self.terminal
-    
+
     def get_last_memory_image(self):
         return self.memory[-1][0]
